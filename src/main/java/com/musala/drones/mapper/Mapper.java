6023e6e7dc0924fa3b43ff32736a0b187707a494
percy <mugadza@gmail.com>
2023-04-17 17:22:48 +0200

    added mapper for medication and dron dtos

--- src/main/java/com/musala/drones/mapper/Mapper.java
+++ src/main/java/com/musala/drones/mapper/Mapper.java
@@ -1,8 +1,19 @@
 package com.musala.drones.mapper;
 
-import org.mapstruct.Mapper;
+import com.musala.drones.drone.Drone;
+import com.musala.drones.drone.dto.DroneDTO;
+import com.musala.drones.medication.Medication;
+import com.musala.drones.medication.dto.MedicationDTO;
 
-@Mapper
-public interface DroneMapper {
-    
+import java.util.Set;
+
+@org.mapstruct.Mapper
+public interface Mapper {
+    Drone toDroneDto(DroneDTO droneDTO);
+    DroneDTO toDrone(Drone drone);
+    MedicationDTO toMedication(Medication medication);
+    Medication toMedicationDto(MedicationDTO medicationDTO);
+    Set<Medication> toMedicationDTOSet(Set<MedicationDTO> medicationDTOSet);
+    Set<MedicationDTO> toMedicationSet(Set<Medication> medicationSet);
+
 }
