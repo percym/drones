eca1d825dceaa449b6e742c6985ec7ea5be42bdc
percy <mugadza@gmail.com>
2023-04-17 23:32:23 +0200

    updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into drone (id,drone_battery_capacity,drone_model,drone_state,drone_weight) values(1, 50,"HeavyWeight","Loading",40)
-insert into drone (id,drone_battery_capacity,drone_model,drone_state,drone_weight) values(2, 60,"LightWeight","Idle",50)
-insert into drone (id,drone_battery_capacity,drone_model,drone_state,drone_weight) values(3, 70,"HeavyWeight","Loading",499)
+insert into drone (id,drone_battery_capacity,drone_model,drone_state,drone_weight) values(1, 50,'Heavyweight','Loading',40);
+insert into drone (id,drone_battery_capacity,drone_model,drone_state,drone_weight) values(2, 60,'Lightweight','Idle',50);
+insert into drone (id,drone_battery_capacity,drone_model,drone_state,drone_weight) values(3, 70,'Heavyweight','Loading',499);
 
-insert into medication (id,medication_code,medication_name,medication_image,medication_weight) values(1,"AAA-1", "Pain Ease","c:\image1.jpg",20)
-insert into medication (id,medication_code,medication_name,medication_image,medication_weight) values(2,"AAA-2", "Cough Buster","c:\image2.jpg",20)
+insert into medication (id,medication_code,medication_name,medication_image,medication_weight) values(1,'AAA-1', 'Pain Ease','c:\image1.jpg',20);
+insert into medication (id,medication_code,medication_name,medication_image,medication_weight) values(2,"AAA-2", 'Cough Buster','c:\image2.jpg',20);
