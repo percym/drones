042fe0c092b0e9aa43d97de416b8878d2de03a92
percy <mugadza@gmail.com>
2023-04-18 11:57:38 +0200

    added swagger -ui

--- src/main/java/com/musala/drones/DronesApplication.java
+++ src/main/java/com/musala/drones/DronesApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableWebMvc
 public class DronesApplication {
 
 	public static void main(String[] args) {
